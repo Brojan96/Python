c9f7578e068df47509c620bd411aa3fb339e0492
Brojan96 <74566560+Brojan96@users.noreply.github.com>
2022-04-25 21:39:29 +0200

    further development

--- Scripts/HangmanGame/hangman_game.py
+++ Scripts/HangmanGame/hangman_game.py
@@ -3,12 +3,28 @@ from curses import wrapper
 import random
 import os
 from time import sleep
+from turtle import color
+import string
 
 #target = random.choice(wordlist.txt)
+guessed_letters = {}
+amount_of_guesses = 0
 
 stdscr = curses.initscr()
+curses.start_color()
+
+
+alphabet = list(string.ascii_uppercase)
+for i in alphabet:
+	guessed_letters[i] = curses.color_pair(3)
+
+#stdscr.addstr (str(guessed_letters))
+#stdscr.refresh()
+#sleep (60)
+
+
 curses.noecho()
-curses.cbreak() #optional, curses will react instantly, no need to press enter
+#curses.cbreak() #optional, curses will react instantly, no need to press enter
 
 def galgen(stdscr, guesses):
 	if (guesses>=1):
@@ -41,21 +57,53 @@ def galgen(stdscr, guesses):
 		stdscr.addstr(2, 7, "\\")
 		stdscr.addstr(2, 9, "/")
 
+def alphabet():	
+	stdscr.addstr(9, 1, "A", guessed_letters["A"])
+	stdscr.addstr(9, 3, "B", guessed_letters["B"])
+	stdscr.addstr(9, 5, "C", guessed_letters["C"])
+	stdscr.addstr(9, 7, "D", guessed_letters["D"])
+	stdscr.addstr(9, 9, "E", guessed_letters["E"])
+	stdscr.addstr(9, 11, "F", guessed_letters["F"])
+	stdscr.addstr(9, 13, "G", guessed_letters["G"])
+	stdscr.addstr(9, 15, "H", guessed_letters["H"])
+	stdscr.addstr(9, 17, "I", guessed_letters["I"])
+	stdscr.addstr(9, 19, "J", guessed_letters["J"])
+	stdscr.addstr(9, 21, "K", guessed_letters["K"])
+	stdscr.addstr(9, 23, "L", guessed_letters["L"])
+	stdscr.addstr(9, 25, "M", guessed_letters["M"])
+	stdscr.addstr(11, 1, "N", guessed_letters["N"])
+	stdscr.addstr(11, 3, "O", guessed_letters["O"])
+	stdscr.addstr(11, 5, "P", guessed_letters["P"])
+	stdscr.addstr(11, 7, "Q", guessed_letters["Q"])
+	stdscr.addstr(11, 9, "R", guessed_letters["R"])
+	stdscr.addstr(11, 11, "S", guessed_letters["S"])
+	stdscr.addstr(11, 13, "T", guessed_letters["T"])
+	stdscr.addstr(11, 15, "U", guessed_letters["U"])
+	stdscr.addstr(11, 17, "V", guessed_letters["V"])
+	stdscr.addstr(11, 19, "W", guessed_letters["W"])
+	stdscr.addstr(11, 21, "X", guessed_letters["X"])
+	stdscr.addstr(11, 23, "Y", guessed_letters["Y"])
+	stdscr.addstr(11, 25, "Z", guessed_letters["Z"])
+	stdscr.refresh()
+
+# FUNKTION die checkt, ob der Input korrekt ist. falls ja: grünen colorcode hinterlegen, falls nein: roten hinterlegen
+# Funktion, die das Lösungswort in unterstrichen darstellt, solange diese noch nicht korrekt geraten wurden. 
+# Wenn sie dann richtig geraten werden, erscheinen die Buchstaben.
 
 
 def main(stdscr):
-	curses.start_color()
 	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
 	stdscr.clear()
-	for i in range(9):
+	alphabet()
+	for i in range(10):
 		sleep(1)
 		galgen(stdscr, i)
 		stdscr.refresh()
 
-	sleep(10)
+	sleep(60)
 
 	"""
 	while guesses < 11:
