166e9b45e9554c9776e1c4f03f7adae0fac286c5
Brojan96 <74566560+Brojan96@users.noreply.github.com>
2022-04-25 19:23:48 +0200

    optimized the code and shortened the script by about 20 lines

--- Scripts/DiceRoller/dice_roller.py
+++ Scripts/DiceRoller/dice_roller.py
@@ -1,62 +1,44 @@
 import random
 import os
+global values
+values = []
 
-def dice_roller() :
-	clearConsole()
+def numberandpositive(x, standardvalue):
 	while True:
-		times = input('How many dices should I roll for you? [1]')
 		try: #validate that the input is numeric and positive
-			if times == '' :
-				times = int(1)
-			times = int(times)
-			if times < 1:
+			if x == '':
+				x = standardvalue
+			y = int(x)
+			if y < 1:
 				clearConsole()
-				print('Please enter a positive number.\n')
+				x=input(f'Please enter a positive number. [{standardvalue}]\n>')
 				continue
+			values.append(y)
 			break
 		except:
 			clearConsole()
 			print('Please enter a numeric value :(\n')
-	while True:
-		low_end = input('Lowest possible number? [1]')
-		try: #validate that the input is numeric and positive
-			if low_end == '' :
-				low_end = int(1)
-			low_end = int(low_end)
-			if low_end < 1:
-				clearConsole()
-				print('Please enter a positive number.\n')
-				continue
-			break
-		except:
-			clearConsole()
-			print('Please enter a numeric value :(\n')
-	while True:
-		high_end = input('Highest possible number? [6]')
-		try: #validate that the input is numeric and positive
-			if high_end == '' :
-				high_end = int(6)
-			high_end = int(high_end)
-			if high_end < 1:
-				clearConsole()
-				print('Please enter a positive number.\n')
-				continue
-			break
-		except:
-			clearConsole()
-			print('Please enter a numeric value :(\n')
-	if times > 1 :
-		counter = 0
-		while counter < times :
-			counter = counter + 1
-			result = int(random.uniform(low_end, high_end))
-			print(result)
-	else :
-		result = int(random.uniform(low_end, high_end))
-		print(result)
+
 def clearConsole():
 	command = 'clear'
 	if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
 		command = 'cls'
 	os.system(command)
+
+def dice_roller() :
+	clearConsole()
+	x = input('How many dices should I roll for you? [1]')
+	numberandpositive(x, 1)
+	clearConsole()
+	x = input('Lowest possible number? [1]')
+	numberandpositive(x, 1)
+	clearConsole()
+	x = input('Highest possible number? [6]')
+	numberandpositive(x, 6)
+	clearConsole()
+	counter = 0
+	while counter < int(values[0]) :
+		counter = counter + 1
+		result = int(random.uniform(int(values[1]), int(values[2])))
+		print(result)
 dice_roller()
