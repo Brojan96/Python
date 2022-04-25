b2a6c6537c2a1d074a1e9279c34f088a90015ed5
Daniel <daniel.oberlaender@campus.tu-berlin.de>
2022-04-25 20:07:38 +0100

    Add galgen function

--- Scripts/HangmanGame/hangman_game.py
+++ Scripts/HangmanGame/hangman_game.py
@@ -2,19 +2,62 @@ import curses
 from curses import wrapper
 import random
 import os
+from time import sleep
 
-target = random.choice(wordlist.txt)
+#target = random.choice(wordlist.txt)
 
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak() #optional, curses will react instantly, no need to press enter
 
+def galgen(stdscr, guesses):
+	if (guesses>=1):
+		stdscr.addstr(5, 1, "/")
+		stdscr.addstr(6, 0, "/")
+	if (guesses >= 2):
+		stdscr.addstr(5, 3, "\\")
+		stdscr.addstr(6, 4, "\\")
+	if (guesses >= 3):
+		stdscr.addstr(4, 2, "|")
+		stdscr.addstr(3, 2, "|")
+		stdscr.addstr(2, 2, "|")
+		stdscr.addstr(1, 2, "|")
+	if (guesses>=4):
+		stdscr.addstr(0, 3, "_")
+		stdscr.addstr(0, 4, "_")
+		stdscr.addstr(0, 5, "_")
+		stdscr.addstr(0, 6, "_")
+		stdscr.addstr(0, 7, "_")
+	if (guesses>=5):
+		stdscr.addstr(1, 8, "|")
+	if (guesses>=6):
+		stdscr.addstr(2, 8, "o")
+	if (guesses>=7):
+		stdscr.addstr(3, 8, "|")
+	if (guesses>=8):
+		stdscr.addstr(4, 7, "/")
+		stdscr.addstr(4, 9, "\\")
+	if (guesses>=9):
+		stdscr.addstr(2, 7, "\\")
+		stdscr.addstr(2, 9, "/")
+
+
+
 def main(stdscr):
+	curses.start_color()
 	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
 	stdscr.clear()
+	for i in range(9):
+		sleep(1)
+		galgen(stdscr, i)
+		stdscr.refresh()
+
+	sleep(10)
+
+	"""
 	while guesses < 11:
 		try:
 			key = stdscr.getkey()
@@ -28,48 +71,7 @@ def main(stdscr):
             continue
 
 		
-		
+"""
 
-def hangman_game() :
-	global answer
-	global hangman_word
-	global guessed_letters
-	global guesses
-	wordlist = open("wordlist.txt", "rt")
-	if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-		wordlist = open("wordlist.txt", "rt").strip()
-	guesses = 0
-	guessed_letters = None
-	hangman_word=','.join(random.choices(wordlist))
-	while guesses < 10 :
-		answer = input('Guess a letter :)\n').lower().strip()
-		if answer == 'clear' :
-			clearConsole()
-			continue
-		elif answer == '' :
-			print('Please enter an alphabetical value.\n')
-			continue
-		elif len(answer) > 1:
-			print('Please enter only one letter.\n')
-			continue
-		elif str.isalpha(answer) == False :
-			print('Please enter an alphabetical value.\n')
-			continue
-		else :
-			game_engine()
-
-def game_engine() :
-	clearConsole()
-	if guessed_letters == hangman_word :
-		guessed_letters = guessed_letters + answer
-		guesses = guesses + 1
-		print(guessed_letters)
-	elif guessed_letters != hangman_word :
-		guesses = guesses + 1
-
-def clearConsole():
-	command = 'clear'
-	if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-		command = 'cls'
-	os.system(command)
-hangman_game()
+if __name__=="__main__":
+	wrapper(main)
