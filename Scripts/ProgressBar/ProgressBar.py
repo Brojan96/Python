0f7d491a0a2e5841082a7bba6b1b3629d4234a3f
Brojan <thekampfkeks96@gmail.com>
2022-06-09 18:37:34 +0200

    minor additions

--- Scripts/ProgressBar/ProgressBar.py
+++ Scripts/ProgressBar/ProgressBar.py
@@ -1,9 +1,12 @@
 import math
+import colorama
 
-def progressBar(progress, total):
+def progressBar(progress, total, color=colorama.Fore.YELLOW):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + '-' * (100 - int(percent)) 
-    print(f'\r|{bar}| {percent:.2f}%', end='\r')
+    print(color + f'\r|{bar}| {percent:.2f}%', end='\r')
+    if progress == total:
+        print (colorama.Fore.GREEN + f'\r|{bar}| {percent:.2f}%', end='\r')
 
 numbers = [x * 5 for x in range(2000, 3000)]
 results = []
@@ -12,3 +15,5 @@ progressBar(0, len(numbers))
 for i, x in enumerate(numbers):
     results.append(math.factorial(x))
     progressBar(i + 1, len(numbers))
+
+print(colorama.Fore.RESET)
